798397ab67e201f4317df6e261dc7f2564a6977e
Ruud Koolen <redlizard@redlizard.nl>
2011-07-06 14:12:01 +0200

    Moved current tree contents to fresh subdirectory.

---
+++ controlpanel/src/config.php
@@ -0,0 +1,14 @@
+<?php
+
+$controlpanelDisabled = false;
+$controlpanelDisabledNotice = "<p>The Treva control panel is currently disabled for maintenance.</p>";
+
+$database_hostname = "145.120.12.40";
+$database_username = "fhp";
+$database_password = "stef45";
+$database_name = "system";
+
+$crypto_key = "aiulfdsfhkasdfouglasdfsdfasdfasjd,fgksdfj";
+
+$ssh_private_key_file = "~/.ssh/id_rsa";
+?>
