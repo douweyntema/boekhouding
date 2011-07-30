b947be64e6d369264fbaf9352cb24fa8b54c0928
Stef Louwers <s.louwers@treva.nl>
2011-07-30 03:59:00 +0200

    added some fields for the mijn domein reseller api

--- controlpanel/src/config.php
+++ controlpanel/src/config.php
@@ -1,5 +1,7 @@
 <?php
 
+$adminMail = "treva@treva.nl";
+
 $controlpanelDisabled = false;
 $controlpanelDisabledNotice = "<p>The Treva control panel is currently disabled for maintenance.</p>";
 
@@ -11,4 +13,10 @@ $database_name = "treva-panel";
 $crypto_key = "aiulfdsfhkasdfouglasdfasjd,fgksdfj";
 
 $ssh_private_key_file = "~/.ssh/id_rsa";
+
+$domainResellerApiUrl = "https://manager.mijndomeinreseller.nl/api/?user=user&pass=pass&authtype=plain";
+$mijnDomainResellerAdminID = 22310;
+$mijnDomainResellerTechID = 22310;
+$mijnDomainResellerBillID = 22310;
+
 ?>
