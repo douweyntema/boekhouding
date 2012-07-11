a9c9b65b1b6457754bcaca120ecd44cd8d33abc3
Stef Louwers <s.louwers@treva.nl>
2012-07-11 17:53:18 +0200

    Regenerate invoices if they do not yet exist.

--- controlpanel/src/billing/invoicepdf.php
+++ controlpanel/src/billing/invoicepdf.php
@@ -8,6 +8,19 @@ function main()
 	doInvoice($invoiceID);
 	
 	$pdf = $GLOBALS["database"]->stdGet("billingInvoice", array("invoiceID"=>$invoiceID), "pdf");
+	if($pdf === null) {
+		$tex = $GLOBALS["database"]->stdGet("billingInvoice", array("invoiceID"=>$invoiceID), "tex");
+		
+		if($tex === null) {
+			billingCreateInvoiceTex($invoiceID, false);
+		} else {
+			billingCreateInvoicePdf($invoiceID, false);
+		}
+		$pdf = $GLOBALS["database"]->stdGet("billingInvoice", array("invoiceID"=>$invoiceID), "pdf");
+		if($pdf === null) {
+			die("This invoice is currently unavailable.");
+		}
+	}
 	
 	header("Content-Type: application/pdf");
 	header("Content-Length: " . strlen($pdf));
