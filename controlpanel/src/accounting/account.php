57a683ec406a2d2ac5d370d295c914d75b30a5fd
Stef Louwers <s.louwers@treva.nl>
2013-04-09 14:29:09 +0200

    moved add transaction box to the top of the account page

--- controlpanel/src/accounting/account.php
+++ controlpanel/src/accounting/account.php
@@ -10,12 +10,12 @@ function main()
 	$content = makeHeader("Account $accountName", accountBreadcrumbs($accountID));
 	
 	$content .= accountSummary($accountID);
-	$content .= transactionList($accountID);
 	if(stdGet("accountingAccount", array("accountID"=>$accountID), "isDirectory")) {
 		$content .= addAccountForm($accountID, "STUB");
 	} else {
 		$content .= addTransactionForm($accountID);
 	}
+	$content .= transactionList($accountID);
 	$content .= editAccountForm($accountID, "STUB");
 	$content .= moveAccountForm($accountID, "STUB");
 	if(accountEmpty($accountID)) {
